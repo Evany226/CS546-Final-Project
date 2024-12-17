929fda10cbd658c7ad0271616888d77551e7e54b
Amy Huang <amy@amys-mbp.mynetworksettings.com>
2024-12-16 23:08:45 -0500

    made a admin only button on collections page

--- routes/collectionRoute.js
+++ routes/collectionRoute.js
@@ -6,10 +6,19 @@ import { Collection } from "mongodb";
 
 router.route("/").get(async (req, res) => {
   try {
+    let admin = false;
+    const user = req.session.user;
+    if(user){
+      if (user.role === "admin"){
+        admin = true;
+      }
+    }
+
     let collectionList = await collectionData.getAllCollections();
     res.render("collections", {
       title: "Collection",
       collection: collectionList,
+      admin: admin
     });
   } catch (e) {
     // console.log(e)
